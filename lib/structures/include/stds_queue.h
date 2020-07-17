42f7e86679278e7a5d92606fd8273a38adcce86e
Joshua Crotts <joshuacrotts@yahoo.com>
2020-07-17 14:48:55 -0400

    Finished stack implementation and almost finished queue.

--- lib/structures/include/stds_queue.h
+++ lib/structures/include/stds_queue.h
@@ -5,16 +5,23 @@
 
 #include "stds_data.h"
 
-typedef struct stds_queue_t stds_queue_t;
+typedef struct stds_queue_t      stds_queue_t;
+typedef struct stds_queue_node_t stds_queue_node_t;
 
-extern void Stds_QueueCreate( struct stds_queue_t *s );
+extern stds_queue_t *Stds_QueueCreate( size_t element_size );
 
-extern void Stds_QueueAdd( struct stds_queue_t *s, void *data );
+extern void Stds_QueueAdd( struct stds_queue_t *q, void *data );
 
-extern void *Stds_QueuePoll( struct stds_queue_t *s );
+extern void *Stds_QueuePoll( struct stds_queue_t *q );
 
-extern void Stds_QueueClear( struct stds_queue_t *s );
+extern void Stds_QueueClear( struct stds_queue_t *q );
 
-extern void *Stds_QueuePeek( struct stds_queue_t *s );
+extern void *Stds_QueuePeek( struct stds_queue_t *q );
+
+extern size_t Stds_QueueSize( struct stds_queue_t *q );
+
+extern bool Stds_QueueIsEmpty( struct stds_queue_t *q );
+
+extern void Stds_QueueDestroy( struct stds_queue_t *q );
 
 #endif // STDS_QUEUE_H
--- lib/structures/include/stds_stack.h
+++ lib/structures/include/stds_stack.h
@@ -7,7 +7,7 @@
 
 typedef struct stds_stack_t stds_stack_t;
 
-extern void Stds_StackCreate( struct stds_stack_t *s );
+extern stds_stack_t *Stds_StackCreate( size_t element_size );
 
 extern void Stds_StackPush( struct stds_stack_t *s, void *data );
 
@@ -16,5 +16,11 @@ extern void *Stds_StackPop( struct stds_stack_t *s );
 extern void Stds_StackClear( struct stds_stack_t *s );
 
 extern void *Stds_StackPeek( struct stds_stack_t *s );
+
+extern size_t Stds_StackSize( struct stds_stack_t *s );
+
+extern bool Stds_StackIsEmpty( struct stds_stack_t *s );
+
+extern void Stds_StackDestroy( struct stds_stack_t *s );
 
 #endif // STDS_STACK_H
