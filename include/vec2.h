d9252a9e05c5af5cbd172ff968e5b58e8ddeca50
m.strahinja <m.strahinja@yahoo.com>
2020-07-31 17:48:40 -0500

    Added vec2 functions.

--- include/vec2.h
+++ include/vec2.h
@@ -23,4 +23,12 @@ extern struct vec2_t Stds_CrossProduct( struct vec2_t *u, struct vec2_t *v );
 
 extern void Stds_NormalizeVec2( struct vec2_t *v );
 
+extern void Stds_AddToVec2( struct vec2_t *v, const float add );
+
+extern void Stds_SubtractToVec2( struct vec2_t *v, const float subtract );
+
+extern void Stds_MultiplyToVec2( struct vec2_t *v, const float multiply );
+
+extern void Stds_DivideToVec2( struct vec2_t *v, const float divide );
+
 #endif
